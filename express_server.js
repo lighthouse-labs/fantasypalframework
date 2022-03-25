55e66c76a0d9d2b9c36011852f0fbb4f182d2fe1
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-25 20:16:44 +0000

    added logic that will generate a new string

--- express_server.js
+++ express_server.js
@@ -1,3 +1,7 @@
+function generateRandomString() {
+  let r = (Math.random().toString(36).substring(6))
+console.log(r);
+}
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -8,6 +12,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,6 +36,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
