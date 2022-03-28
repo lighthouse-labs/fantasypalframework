900a4cac91d156cb73144120bbed1d25be2beca2
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-28 17:51:07 +0000

    fixed login issues

--- express_server.js
+++ express_server.js
@@ -30,17 +30,21 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, "username": req.cookies.username };
+  let templateVars = { urls: urlDatabase, username: req.cookies['username'] };
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls.json", (req, res) => {
+  res.send(urlDatabase);
+})
+
 app.get("/urls/new", (req, res) => {
-  let templateVars = { urls: urlDatabase, "username": req.cookies.username };
+  let templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render('urls_new', templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username']};
   res.render("urls_show", templateVars);
 });
 
@@ -59,7 +63,8 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect('/urls_show', templateVars);
 })
 
-app.post("/login", (res,req) => {
+app.post("/login", (req,res) => {
+  console.log(req.body);
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 })
