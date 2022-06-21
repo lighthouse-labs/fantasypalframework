7ad5cd196cdf705a8ef994ab33bf6424c8675df1
Nathan Bain <nate.bain9@gmail.com>
2022-06-20 22:47:37 -0400

    add footer

--- helpers.js
+++ helpers.js
@@ -25,5 +25,60 @@ function urlsForUser(id, urlDatabase) {
   return userUrls;
 }
 
+const ANAsched = require('./db/ANAsched')
+const ARIsched = require('./db/ARIsched')
+const BOSsched = require('./db/BOSsched')
+const BUFsched = require('./db/BUFsched')
+const CALsched = require('./db/CALsched')
+const CARsched = require('./db/CARsched')
+const CHIsched = require('./db/CHIsched')
+const COLsched = require('./db/COLsched')
+const CBJsched = require('./db/CBJsched')
+const DALsched = require('./db/DALsched')
+const DETsched = require('./db/DETsched')
+const EDMsched = require('./db/EDMsched')
+const FLOsched = require('./db/FLOsched')
+const LAKsched = require('./db/LAKsched')
+const MINsched = require('./db/MINsched')
+const MTLsched = require('./db/MTLsched')
+const NASsched = require('./db/NASsched')
+const NJDsched = require('./db/NJDsched')
+const NYIsched = require('./db/NYIsched')
+const NYRsched = require('./db/NYRsched')
+const OTTsched = require('./db/OTTsched')
+const PHIsched = require('./db/PHIsched')
+const PITsched = require('./db/PITsched')
+const SJSsched = require('./db/SJSsched')
+const SEAsched = require('./db/SEAsched')
+const STLsched = require('./db/STLsched')
+const TBLsched = require('./db/TBLsched')
+const TMLsched = require('./db/TMLsched')
+const VANsched = require('./db/VANsched')
+const VGKsched = require('./db/VGKsched')
+const WASsched = require('./db/WASsched')
+const WINsched = require('./db/WINsched')
+
+
+
+
+const scheduleComparison = (arr1, arr2, date) => {
+  const sameDays = [];  // Array to contain match elements
+  for(var i=0 ; i<arr1.length ; ++i) {
+    for(var j=0 ; j<arr2.length ; ++j) {
+      if (date >= arr1[i]) { // only access numbers greater than current date (later days)
+        
+      } else {
+      if(arr1[i] == arr2[j]) {    // If element is in both the arrays
+        sameDays.push(arr1[i]);        // Push to arr array
+        }
+      }
+    }
+  }
+   
+  return sameDays.length;  // Return the number of same days played on in sameDays 
+}  
+
+module.exports = scheduleComparison
+
 
 module.exports = { emailCheck, generateRandomString, urlsForUser }
